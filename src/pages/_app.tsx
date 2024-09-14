23a8523c232ef49998353c8462568220787d7686
PlayySocial <143432967+playysocial@users.noreply.github.com>
2024-09-15 02:48:10 +0530

    TS FI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "../i18n";
 import MusicWidget from "../components/MusicWidget";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isLoading, setIsLoading] = useState(true);
